613e217be1e2ebb52e49c4aed13036a292452369
steph <stephanie_sarvis@msn.com>
2019-03-27 21:05:24 -0400

    bloc jams sliders assignment and extra credit completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,6 +1,6 @@
 {
   $('button#play-pause').on('click', function() {
-    player.playPause();
+    helper.playPauseAndUpdate();
     $(this).attr('playState', player.playState);
   });
 
@@ -10,7 +10,7 @@
     const nextSongIndex = currentSongIndex +1;
     if (nextSongIndex >= album.songs.length) { return; }
     const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
+    helper.playPauseAndUpdate(nextSong);
   });
 
   $('button#previous').on('click', function() {
@@ -19,11 +19,15 @@
     const nextSongIndex = currentSongIndex -1;
     if (nextSongIndex < 0) { return; }
     const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
+    helper.playPauseAndUpdate(nextSong);
   });
 
-  $('time-control input').on('input', function (event) {
+  $('#time-control input').on('input', function (event) {
     player.skipTo(event.target.value);
+  });
+
+  $('#volume-control input').on('input', function (event) {
+    player.setVolume(event.target.value);
   });
 
   setInterval( () => {
@@ -31,7 +35,7 @@
     const currentTime = player.getTime();
     const duration = player.getDuration();
     const percent = (currentTime / duration) * 100;
-    $('#time-control .current-time').text( currentTime );
+    $('#time-control .current-time').text( player.prettyTime(currentTime) );
     $('#time-control input').val(percent);
   }, 1000);
 }
